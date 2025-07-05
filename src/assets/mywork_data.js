e2c70ad4d2a6350c5eaf44781a73ce6b27a5e0cf
Sujal Rijal <lsuja502@gmail.com>
2025-07-06 00:25:51 +0545

    Update mywork_data and add new dashboard images

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -2,8 +2,8 @@ import project1_img from '../assets/dashboard_1.png';
 import project2_img from '../assets/dashboard_2.png';
 import project3_img from '../assets/dashboard_3.png';
 import project4_img from '../assets/dashboard_4.png';
-import project5_img from '../assets/dashboard_2.png'; 
-import project6_img from '../assets/dashboard_1.png'; 
+import project5_img from '../assets/dashboard_5.png'; 
+import project6_img from '../assets/dashboard_6.png'; 
 
 const mywork_data = [
   {
